4f4503c280919fd39472a5b07add547ef24fe8c2
Urvil Patel <uvpatel7271@gmail.com>
2026-08-20 22:30:50 +0530

    schema auth and route implemented

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,18 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db"; // your drizzle instance
+import { db } from "@/db";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-    }),
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+        clientId: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }
+  }
 });
